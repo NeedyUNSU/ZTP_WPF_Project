b7909c53b17f8dc36e6ecfd74f29620e3ce8e282
zygi1845 <zygmunt1845mankowski@gmail.com>
2025-01-17 11:19:43 +0100

    class

---
+++ Builder/Directory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZTP_WPF_Project.Builder
+{
+    internal class Directory
+    {
+    }
+}
--- MVVM/Model/ReportModel.cs
+++ MVVM/Model/ReportModel.cs
@@ -8,6 +8,15 @@ namespace ZTP_WPF_Project.MVVM.Model
 {
     public class ReportModel : BaseModel
     {
+        
+        public DateTime CreatedDate { get; set; } = DateTime.Now; // Data utworzenia raportu.
+        public decimal TotalIncome { get; set; } // Całkowite przychody (tylko do odczytu).
+        public decimal TotalExpenses { get; set; } // Całkowite wydatki (tylko do odczytu).
+        public decimal MaxIncome { get; set; } // maksymalny przychód
+        public decimal MaxExpenses { get; set; } //maksymalny wydatek
+        public decimal Amount_income { get; set; } //ilość przychodów
+        public decimal Amount_expenses { get; set; } //ilość wydatków
+
         public List<TransactionModel>? transactions;
 
         public override bool Validate()
